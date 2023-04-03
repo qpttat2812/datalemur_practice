9aa53190d8f2f5640781fbd7b0234393f53be162
unknown <quynhphamsek@gmail.com>
2023-04-03 23:25:46 +0700

    update topping pizza

--- 3-Topping Pizzas.sql
+++ 3-Topping Pizzas.sql
@@ -8,4 +8,11 @@ select DISTINCT concat(p1.topping_name, ',', p2.topping_name, ',', p3.topping_na
 		p1.ingredient_cost + p2.ingredient_cost + p3.ingredient_cost as total_cost
 from tb1 p1, tb1 p2, tb1 p3
 where p1.rnw_order < p2.rnw_order and  p2.rnw_order < p3.rnw_order
-order by total_cost desc
+order by total_cost desc;
+
+select
+  concat(pt1.topping_name, ',', pt2.topping_name, ',', pt3.topping_name) as pizza,
+  pt1.ingredient_cost+pt2.ingredient_cost+pt3.ingredient_cost as total_cost
+from pizza_toppings as pt1, pizza_toppings as pt2, pizza_toppings as pt3
+where pt1.topping_name<pt2.topping_name and pt2.topping_name<pt3.topping_name 
+order by 2 desc;
